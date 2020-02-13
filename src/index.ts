b57908b748df237c6a0d1a5e3bd5a0c13537065b
Tim Graboski <tgraboski@Tims-MBP.tutela.tut>
2020-02-13 13:18:30 -0800

    trying to get a grid to work.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-import { Color4Buffer, FloatBuffer, IndexBuffer } from "./Buffer";
+import { Color4Buffer, FloatBuffer, IndexBuffer, Vector3Buffer } from "./Buffer";
 import { Color } from "./Color";
 import { Shader } from "./Shader";
+import { Vector3 } from "./Vector3";
+import { LookAtCamera, Camera, PerspectiveLens, Lens } from "./Camera";
+import { mat4 } from "gl-matrix";
 
 main();
 
@@ -12,8 +15,11 @@ function main() {
   if (!canvas){
     return;
   }
-  canvas.setAttribute("width", `${window.innerWidth}px`);
-  canvas.setAttribute("height", `${window.innerHeight}px`);
+
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  canvas.setAttribute("width", `${width}px`);
+  canvas.setAttribute("height", `${height}px`);
   const gl = canvas.getContext('webgl2') as WebGL2RenderingContext;
 
   // If we don't have a GL context, give up now
@@ -23,10 +29,17 @@ function main() {
   }
 
   const background = initBackground(gl);
+  const grids = initGrids(gl);
+  const camera = new LookAtCamera();
+  camera.setPosition(new Vector3(10, 11, 12));
+  camera.setTarget(new Vector3(0, 0, 0));
+  camera.setUp(new Vector3(0, 1, 0))
+  const lens = new PerspectiveLens();
+  lens.aspect = width / height;
 
   // Draw the scene repeatedly
   function render() {
-    drawScene(gl, background);
+    drawScene(gl, background, grids, camera, lens);
 
     requestAnimationFrame(render);
   }
@@ -95,10 +108,74 @@ function initBackground(gl: WebGL2RenderingContext){
   };
 }
 
+function initGrids(gl: WebGL2RenderingContext){
+  const gridPositions: Vector3[] = [];
+  const indexPositions: number[] = [];
+
+  let i: number;
+  for (i = -5; i <= 5; i++){
+    gridPositions.push(new Vector3(i, -5, 0));
+    gridPositions.push(new Vector3(i, 5, 0));
+  }
+
+  for (i = -4; i < 5; i++){
+    gridPositions.push(new Vector3(-5, i, 0));
+    gridPositions.push(new Vector3(5, i, 0));
+  }
+
+  for (i = 0; i < 24; i++){
+    indexPositions.push(i);
+  }
+  indexPositions.push(0);
+  indexPositions.push(23);
+  for (i = 24; i < 40; i++) {
+    indexPositions.push(i);
+  }
+
+  console.log(gridPositions);
+  console.log(indexPositions);
+
+  const gridShader = new Shader(gl)
+    .addVertexSource(`
+precision lowp float;
+
+uniform vec4 color;
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+attribute vec4 vertexPosition;
+
+void main(void) {
+  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vertexPosition;
+}
+    `)
+    .addFragmentSource(`
+precision lowp float;
+
+uniform vec4 color;
+
+void main(void) {
+  gl_FragColor = color;
+}
+    `)
+    .link();
+
+  return {
+    program: gridShader.getProgram(),
+    positionAttribute: gridShader.getAttributeLocation("vertexPosition"),
+    colorUniform: gridShader.getUniformLocation("color"),
+    modelMatrixUniform: gridShader.getUniformLocation("modelMatrix"),
+    viewMatrixUniform: gridShader.getUniformLocation("viewMatrix"),
+    projectionMatrixUniform: gridShader.getUniformLocation("projectionMatrix"),
+    positionBuffer: new Vector3Buffer(gl, gridPositions),
+    indexBuffer: new IndexBuffer(gl, indexPositions),
+  }
+}
+
 //
 // Draw the scene.
 //
-function drawScene(gl: WebGL2RenderingContext, background: any) {
+function drawScene(gl: WebGL2RenderingContext, background: any, grids: any, camera: Camera, lens: Lens) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
   gl.enable(gl.DEPTH_TEST);           // Enable depth testing
@@ -107,26 +184,66 @@ function drawScene(gl: WebGL2RenderingContext, background: any) {
   // Clear the canvas before we start drawing on it.
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  const {
-    vertexPositionAttribute,
-    color1Uniform,
-    color2Uniform,
-    color3Uniform,
-    color4Uniform,
-    program,
-    positionBuffer,
-    indexBuffer,
-  } = background;
+  {
+    const {
+      vertexPositionAttribute,
+      color1Uniform,
+      color2Uniform,
+      color3Uniform,
+      color4Uniform,
+      program,
+      positionBuffer,
+      indexBuffer,
+    } = background;
 
-  (positionBuffer as FloatBuffer).bindToAttribute(vertexPositionAttribute);
-  (indexBuffer as IndexBuffer).bindToAttribute();
+    (positionBuffer as FloatBuffer).bindToAttribute(vertexPositionAttribute);
+    (indexBuffer as IndexBuffer).bindToAttribute();
 
-  // Tell WebGL to use our program when drawing
+    // Tell WebGL to use our program when drawing
 
-  gl.useProgram(program);
-  gl.uniform4fv(color1Uniform, Color.fromHex("#0182B2"));
-  gl.uniform4fv(color2Uniform, Color.fromHex("#EC4980"));
-  gl.uniform4fv(color3Uniform, Color.fromHex("#FFDA8A"));
-  gl.uniform4fv(color4Uniform, Color.fromHex("#50377E"));
-  gl.drawElements(gl.TRIANGLE_STRIP, 4, gl.UNSIGNED_SHORT, 0);
+    gl.useProgram(program);
+    gl.uniform4fv(color1Uniform, Color.fromHex("#0182B2"));
+    gl.uniform4fv(color2Uniform, Color.fromHex("#EC4980"));
+    gl.uniform4fv(color3Uniform, Color.fromHex("#FFDA8A"));
+    gl.uniform4fv(color4Uniform, Color.fromHex("#50377E"));
+    gl.drawElements(gl.TRIANGLE_STRIP, 4, gl.UNSIGNED_SHORT, 0);
+  }
+
+  gl.clearDepth(1.0);                 // Clear everything
+  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+  {
+    const {
+      program,
+      positionAttribute,
+      colorUniform,
+      positionBuffer,
+      indexBuffer,
+      modelMatrixUniform,
+      viewMatrixUniform,
+      projectionMatrixUniform,
+    } = grids;
+
+    const modelMatrix = mat4.create();
+    const viewMatrix = camera.getViewMatrix();
+    const projectionMatrix = lens.getProjection();
+
+    gl.useProgram(program);
+    gl.uniform4fv(colorUniform, Color.fromHex("#efebeb"));
+    (positionBuffer as FloatBuffer).bindToAttribute(positionAttribute);
+    (indexBuffer as IndexBuffer).bindToAttribute();
+    gl.uniformMatrix4fv(
+      modelMatrixUniform,
+      false,
+      modelMatrix);
+    gl.uniformMatrix4fv(
+      viewMatrixUniform,
+      false,
+      viewMatrix);
+    gl.uniformMatrix4fv(
+      projectionMatrixUniform,
+      false,
+      projectionMatrix);
+    gl.drawElements(gl.LINES, (indexBuffer as IndexBuffer).getLength(), gl.UNSIGNED_SHORT, 0);
+  }
 }
