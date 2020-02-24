271d19f05d5476cf8388ddda8111d94cbf9d546d
Tim Graboski <tgraboski@Tims-MBP.tutela.tut>
2020-02-24 08:57:45 -0800

    Adding tslint, husky, lint-staged, and prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    arrowParens: "always",
+    bracketSpacing: false,
+    endOfLine: "lf",
+    jsxBracketSameLine: false,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: "es5",
+    printWidth: 100,
+};
