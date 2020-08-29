6389e8532b61f235d0691105011991d7ea62693a
whiterook6 <whiterook6@gmail.com>
2020-08-28 20:09:06 -0700

    Moving shader code into reusable renderers

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import {mat4} from "gl-matrix";
 import {AnimationLoop, ITimestamp} from "./animation";
-import {Camera, PerspectiveLens} from "./cameras";
+import {PerspectiveLens} from "./cameras";
 import {OrbitCamera} from "./cameras/OrbitCamera";
 import {IMouseDrag, Mouse} from "./interaction/Mouse";
 import {Color4Bezier, loop, pipe, sin, transform} from "./interpolators";
 import {FullscreenQuad} from "./objects/FullscreenQuad";
 import {Gizmo} from "./objects/Gizmo";
-import {Sphere} from "./objects/Sphere";
+import {Color} from "./types";
 import {ThreeDGrid} from "./objects/ThreeDGrid";
-import {Color, Plane, ray} from "./types";
-
-main();
 
 //
 // Start here
@@ -82,17 +79,6 @@ function main() {
   const blPipe = pipe([loop(0, 5000), transform(0.0002), sin], blBezier.get);
   const brPipe = pipe([loop(0, 5000), transform(0.0002), sin], brBezier.get);
 
-  const grids = new ThreeDGrid(gl);
-  const sphere = new Sphere(gl, 64);
-  const sphereModelMatrix = mat4.create();
-  // const texturedCube = new TexturedCube(gl);
-  // const framebuffer = new Framebuffer(gl, 256, 256);
-
-  // const camera = new OrbitCamera();
-  // camera.setDistance(10);
-  // camera.setTheta(-Math.PI / 12);
-  // camera.setTarget(new Vector3(0, 0, 0));
-  // camera.setUp(new Vector3(0, 1, 0));
   const sceneCamera = new OrbitCamera();
   sceneCamera.setDistance(10);
   sceneCamera.setTheta(-Math.PI / 12);
@@ -100,27 +86,11 @@ function main() {
   sceneCamera.setUp([0, 0, 1]);
 
   const lens = new PerspectiveLens();
+  const grids = new ThreeDGrid(gl);
   const gizmo = new Gizmo(gl);
 
   function render(timestamp: ITimestamp) {
     const modelMatrix = mat4.create();
-
-    // framebuffer.render((bufferWidth: number, bufferHeight: number) => {
-    //   const angle = (timestamp.age / 100) * (Math.PI / 180);
-    //   camera.setPhi(angle);
-    //   const _viewMatrix = camera.getViewMatrix();
-
-    //   gl.viewport(0, 0, bufferWidth, bufferHeight);
-    //   gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-    //   gl.clearDepth(1.0); // Clear everything
-    //   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    //   gl.enable(gl.DEPTH_TEST); // Enable depth testing
-    //   gl.depthFunc(gl.LEQUAL); // Near things obscure far things
-    //   lens.aspect = bufferWidth / bufferHeight;
-    //   const _projectionMatrix = lens.getProjection();
-    //   grids.render(modelMatrix, _viewMatrix, _projectionMatrix);
-    //   sphere.render(modelMatrix, _viewMatrix, _projectionMatrix);
-    // });
 
     if (mustResize) {
       mustResize = false;
@@ -132,8 +102,6 @@ function main() {
 
     // normal time
     lens.aspect = width / height;
-    const viewMatrix = sceneCamera.getViewMatrix();
-    const projectionMatrix = lens.getProjection();
     gl.viewport(0, 0, width, height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
     gl.clearDepth(1.0); // Clear everything
@@ -149,14 +117,12 @@ function main() {
     gl.enable(gl.DEPTH_TEST); // Enable depth testing
     gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
-    grids.render(modelMatrix, viewMatrix, projectionMatrix);
-    sphere.render(sphereModelMatrix, viewMatrix, projectionMatrix);
-
+    grids.render(sceneCamera.getViewMatrix(), lens.getProjection());
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
     gl.viewport(width - 200, 0, 200, 200);
     lens.aspect = 1;
     sceneCamera.setDistance(3);
-    gizmo.render(modelMatrix, sceneCamera.getViewMatrix(), lens.getProjection());
+    gizmo.render(sceneCamera.getViewMatrix(), lens.getProjection());
     sceneCamera.setDistance(10);
     lens.aspect = width / height;
     gl.viewport(0, 0, width, height);
@@ -181,20 +147,10 @@ function main() {
 
     const {deltaX, deltaY} = event;
     sceneCamera.movePhi(deltaX * 0.01);
-    sceneCamera.moveTheta(deltaY * 0.01);
+    sceneCamera.moveTheta(-deltaY * 0.01);
   };
   mouse.addDragCallback(moveCamera);
-  document.addEventListener("mousedown", (event) => {
-    const {clientX, clientY} = event;
-    const viewport: [number, number, number, number] = [0, 0, width, height];
-
-    const projViewMatrix = mat4.create();
-    mat4.multiply(projViewMatrix, lens.getProjection(), sceneCamera.getViewMatrix());
-    const r: ray = Camera.unprojectRay([clientX, clientY], viewport, projViewMatrix);
-    const spherePos = Plane.getIntersection(Plane.in([-5, 0, 0]), r);
-    if (spherePos) {
-      mat4.fromTranslation(sphereModelMatrix, spherePos);
-    }
-  });
   mouse.register();
 }
+
+main();
