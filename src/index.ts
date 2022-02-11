eb7930dca915e7c8686c3c42a1ad818701d71af0
Tim Graboski <tgraboski@Tims-MBP.tutela.tut>
2022-02-10 19:42:17 -0800

    Adding simple triangle for testing

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {FullscreenQuad} from "./objects/FullscreenQuad";
 import {Gizmo} from "./objects/Gizmo";
 import {RenderableBezier} from "./objects/RenderableBezier";
 import {ThreeDGrid} from "./objects/ThreeDGrid";
+import {Triangle} from "./objects/Triangle";
 import {BezierPhysicsVerlet} from "./physics/BezierPhysicsVerlet";
 import {VertexColorRenderer} from "./renderers/VertexColorRenderer";
 import {Color, Vector3, vector3} from "./types";
@@ -84,33 +85,9 @@ function main() {
   const blPipe = pipe([loop(0, 5000), transform(0.0002), sin], blBezier.get);
   const brPipe = pipe([loop(0, 5000), transform(0.0002), sin], brBezier.get);
 
-  const sceneCamera = new OrbitCamera();
-  sceneCamera.setDistance(20);
-  sceneCamera.setTheta(-Math.PI / 12);
-  sceneCamera.setTarget([0, 0, 5]);
-  sceneCamera.setUp([0, 0, 1]);
-  const gizmoCamera = new OrbitCamera();
-  gizmoCamera.setDistance(3);
-  gizmoCamera.setTheta(-Math.PI / 12);
-  gizmoCamera.setTarget([0, 0, 0]);
-  gizmoCamera.setUp([0, 0, 1]);
-
-  const lens = new PerspectiveLens();
-  const gizmo = new Gizmo(gl);
-  const bezier = new Vector3Bezier([0, 0, 9], [0, 10, 1], [3, -5, 1], [4, 7, 10]);
-  const renderableBezier = new RenderableBezier(gl, bezier);
-  const grid = new ThreeDGrid(gl);
-
-  const carPhysics = new BezierPhysicsVerlet(bezier);
-  const carVertices = new Vector3Buffer(gl, [[0, 0, 0]]);
-  const carColors = new Color4Buffer(gl, [Color.fromHex("#FFFFFF")]);
-  const carIndices = new IndexBuffer(gl, [0]);
-  const carRenderer = new VertexColorRenderer(gl);
-  const carMatrix = mat4.create();
+  const triangle = new Triangle(gl);
 
   function render(timestamp: ITimestamp) {
-    carPhysics.update(timestamp.deltaT / 1000);
-
     if (mustResize) {
       mustResize = false;
       canvas.setAttribute("width", `${newWidth * devicePixelRatio}px`);
@@ -120,7 +97,6 @@ function main() {
     }
 
     // normal time
-    lens.aspect = width / height;
     gl.viewport(0, 0, width * devicePixelRatio, height * devicePixelRatio);
     gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
     gl.clearDepth(1.0); // Clear everything
@@ -136,28 +112,9 @@ function main() {
     gl.enable(gl.DEPTH_TEST); // Enable depth testing
     gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
-    const viewMatrix = sceneCamera.getViewMatrix();
-    const projectionMatrix = lens.getProjection();
-    grid.render(viewMatrix, projectionMatrix);
-    renderableBezier.render(viewMatrix, projectionMatrix);
-    const carPosition = carPhysics.getPosition();
-    mat4.fromTranslation(carMatrix, carPosition);
-    mat4.multiply(carMatrix, viewMatrix, carMatrix);
-    mat4.multiply(carMatrix, projectionMatrix, carMatrix);
-    carRenderer.render(carVertices, carColors, carIndices, carMatrix, gl.POINTS);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.viewport(
-      width * devicePixelRatio - 200 * devicePixelRatio,
-      0,
-      200 * devicePixelRatio,
-      200 * devicePixelRatio
-    );
-    lens.aspect = 1;
-    gizmoCamera.setPhi(sceneCamera.getPhi());
-    gizmoCamera.setTheta(sceneCamera.getTheta());
-    gizmo.render(gizmoCamera.getViewMatrix(), lens.getProjection());
-    lens.aspect = width / height;
-    gl.viewport(0, 0, width, height);
+    const identity = mat4.create();
+    mat4.identity(identity);
+    triangle.render(identity, identity, identity);
   }
 
   const looper = new AnimationLoop(render);
@@ -170,27 +127,6 @@ function main() {
     }
   });
   looper.resume();
-
-  const mouse = new Mouse();
-  const moveCamera = (event: IMouseDrag) => {
-    if (looper.getIsPaused()) {
-      return;
-    }
-
-    const {deltaX, deltaY} = event;
-    sceneCamera.movePhi(deltaX * -0.01);
-    sceneCamera.moveTheta(deltaY * 0.01);
-  };
-  const zoomCamera = (delta: number) => {
-    if (looper.getIsPaused()) {
-      return;
-    }
-
-    sceneCamera.setDistance(sceneCamera.getDistance() + delta);
-  };
-  mouse.addDragCallback(moveCamera);
-  mouse.addWheelCallback(zoomCamera);
-  mouse.register();
 }
 
 main();
